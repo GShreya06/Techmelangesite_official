9c3f989357687a467a074eb3b2e32aae2a844dca
Shreya <shreyagupta4563@gmail.com>
2022-10-27 17:38:26 +0530

    Initial commit

--- techmelangeproject/home/models.py
+++ techmelangeproject/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    name=models.CharField(max_length=122)
+    email=models.CharField(max_length=122)
+    collegenm=models.CharField(max_length=122)
+    events=models.CharField(max_length=122)
