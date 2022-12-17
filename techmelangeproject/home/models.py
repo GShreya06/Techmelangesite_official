758d9d1552c148bc4b17342e317733a187f33d34
Shreya <shreyagupta4563@gmail.com>
2022-12-17 20:01:05 +0530

    Added files

--- techmelangeproject/home/models.py
+++ techmelangeproject/home/models.py
@@ -10,10 +10,12 @@ class Registration(models.Model):
 class festdesc(models.Model):
     fest_name=models.CharField(max_length=50)
     fest_desc=models.CharField(max_length=100)
+    fest_bg=models.ImageField(null=True)
     
 class events(models.Model):
     e_name=models.CharField(max_length=50)
     e_desc=models.CharField(max_length=100)
     e_img=models.ImageField()
+    e_frm=models.URLField(max_length=200,null=True)
     
      
