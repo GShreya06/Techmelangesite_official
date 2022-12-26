3aeabf800eb2edcf13b695f180dd6baacb0c7b4a
Shreya <shreyagupta4563@gmail.com>
2022-12-26 23:21:53 +0530

    Code -up to date

--- techmelangeproject/home/models.py
+++ techmelangeproject/home/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Shalini Added 
 class festdesc(models.Model):
-    fest_name=models.CharField(max_length=50)
-    fest_desc=models.TextField(max_length=1000)
+    fest_name=models.CharField(max_length=2000)
+    fest_desc=models.TextField(max_length=5000)
 
 
 class sponsers(models.Model):
@@ -23,12 +23,18 @@ class teachcoor(models.Model):
 # Shreya Added 
 class events(models.Model):
     e_name=models.CharField(max_length=50)
-    e_desc=models.CharField(max_length=2000)
+    e_desc=models.CharField(max_length=1000)
     e_img=models.ImageField()
     e_frm=models.URLField(max_length=200,null=True)
     
 class contacts(models.Model):
     C_email=models.CharField(max_length=50)
     C_mobile=models.CharField(max_length=10,null=True)
+
+class main(models.Model):
+    mname=models.CharField(max_length=50)
+    mdesc=models.TextField(max_length=1000)
+    mdate=models.CharField(max_length=50)
+    mbg=models.ImageField()
    
      
--- techmelangeproject/home/views.py
+++ techmelangeproject/home/views.py
@@ -7,44 +7,53 @@ from .models import festdesc
 from .models import events
 from .models import contacts
 from .models import sponsers
+from .models import main
 # Create your views here.
 
 
 def home(request):
+    # for main database
+    m=main.objects.all();
+    print(m)
+    for i in m:
+        m1=i.mname
+        m2=i.mdesc
+        m3=i.mdate
+        m4=i.mbg
     # for only first element use first
     #for all objects use all
-    a=festdesc.objects.all().first(); 
-    print(a)
-    # print(a1)
-    # for i in a: 
-    #     # for taking values from database use loop
-    #     print(i.fest_name) 
-    #     print(i.fest_desc)
-    #     print(i.fest_bg)
     
+    # for about database
+    a=festdesc.objects.all(); 
+    print(a)
+    for i in a: 
+        # for taking values from database use loop
+        a1=i.fest_name 
+        a2=i.fest_desc
+    
+    # for events database
     b=events.objects.all();
     print(b)
     for i in b:
         e1=i.e_name
-        print(i.e_desc)
-        print(i.e_img)
-        print(i.e_frm)
-        
     
-    
-
-    str = "ENIAC-The Computer Science Society, Department of Computer Science, Shaheed Rajguru College Of Applied Sciences for Women, University Of Delhi presents its Annual Techfest - TECH MELANGE'23."
-    str1 = "As the name suggests, Tech Melange is the perfect blend of hosting both technical and non-technical events which means that it is a fun extravaganza for all the tech maniacs as well as the non-tech mob around the corner looking for opportunities and learnings. It was started in the year 2010 and every year thereafter, different sets of events were organised by the departmental council to instil an element of competition in technology; making it an adventurous ride of coding, technical tools, quizzes, brainstorming games and ideas organised by students."
-    str2 = "For the first time in 11 years, Tech Melange was conducted online which resulted in an overwhelming participation from students all over India. It brings together tech-enthusiasts, coders and gamers. Winners are also awarded with lucrative cash prizes, coupons and certificates. Ite is acquainted to the masses by the mingled effort of  the faculty and students."
-    str3 ="To maintain the legacy, we have some exciting events this year as well such as Ideathon, Brain Wreck, E - tambola, Cyber hustle, Whiz Quiz and Hardcode. This gives students a golden opportunity to showcase their talent, innovation, creativity and technicality. Students of various renowned universities and colleges(Delhi University, JNU, IP University, IT, Amity, IIIT-D, Jamia Milia etc.) participate in this mass endeavour."
-    str4="Our aim is to enhance the intellectual awareness and development of students. The fest adopts a highly participative and interactive approach with a strong emphasis on ‘Learning by Doing’ and is successful in quenching the thirst of today’s well aware techno youth all around."
-    email="eniac@rajguru.com"
-    mobile="9814739274, 011-22353108"
-    return render(request, 'index.html', {'stg': str, 'stg1': str1, 'stg2': str2,'stg3':str3 , 
-                  'stg4': str4, 'email': email, 'mobile': mobile,'a':a , 'b':b , 'e1':e1})
+    c=contacts.objects.all();
+    print(c)
+    for i in c:
+        c1=i.C_email
+        c2=i.C_mobile
+    return render(request, 'index.html', {'m1' : m1 ,'m2' : m2 ,'m3' : m3 ,'a1':a1 , 'a2':a2,'b':b ,
+                'e1':e1 , 'c1':c1 , 'c2':c2})
 
 def Hardcode(request):
-    return render (request,'Hardcode.html')
+    b=events.objects.all();
+    print(b)
+    for i in b:
+        e1=i.e_name
+        e2=i.e_desc
+        e3=i.e_img
+        print(i.e_frm)
+    return render (request,'Hardcode.html',{'e1' : e1 , 'e2' : e2 ,'e3' :e3})
 
 def design(request):
     return render (request,'design.html')
