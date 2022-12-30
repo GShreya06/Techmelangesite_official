0782a64f703a94828af2cd9b2cccc866d631662b
Shreya <shreyagupta4563@gmail.com>
2022-12-30 12:52:51 +0530

    Final event changes

--- techmelangeproject/home/urls.py
+++ techmelangeproject/home/urls.py
@@ -8,16 +8,8 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path("sponsers/",views.sponsers,name="sponsers"),
-    path("event/event_id=<event_id>",views.event,name='event'),
-=======
     path("sponsers/",views.sponsers, name='Sponsers'),
     path("event/event_id=<event_id>",views.event,name='event'),
-    #for images
-    # path('admin/',admin.site.urls),
-    # path('', include ('b.urls')),
->>>>>>> 92e33cf4995f2c6bf555dca59557769aac4702bb
 ]
 
 #for images
--- techmelangeproject/home/views.py
+++ techmelangeproject/home/views.py
@@ -33,29 +33,16 @@ def home(request):
     
     # for events database
     b=events.objects.all();
-<<<<<<< HEAD
-    print(b)
-    
-    # remove and pass b
-    # for i in b:
-    #     e1=i.e_name
-=======
     #print(b)
     for i in b:
          e1=i.e_name
->>>>>>> 92e33cf4995f2c6bf555dca59557769aac4702bb
     
     c=contacts.objects.all();
     #print(c)
     for i in c:
         c1=i.C_email
         c2=i.C_mobile
-<<<<<<< HEAD
-    return render(request, 'index.html',{'m1' : m1 ,'m2' : m2 ,'m3' : m3 ,'a1':a1 , 'a2':a2,'b':b ,
-                 'c1':c1 , 'c2':c2} )
-=======
     return render(request, 'index.html', {'m1' : m1 ,'m2' : m2 ,'m3' : m3 ,'a1':a1 , 'a2':a2,'b':b ,'e1':e1 , 'c1':c1 , 'c2':c2})
->>>>>>> 92e33cf4995f2c6bf555dca59557769aac4702bb
 
 def event(request,event_id):
     b=events.objects.get(id=event_id);
@@ -64,36 +51,3 @@ def event(request,event_id):
 
 def sponsers(request):
     return render (request,'sponsers.html')
-
-# def Hardcode(request):
-#     b=events.objects.all();
-#     #print(b)
-#     for i in b:
-#         e1=i.e_name
-#         e2=i.e_desc
-#         e3=i.e_img
-#         #print(i.e_frm)
-#     return render (request,'Hardcode.html',{'e1' : e1 , 'e2' : e2 ,'e3' :e3})
-
-<<<<<<< HEAD
-def sponsers(request):
-    return render (request,'sponsers.html')
-=======
-# def design(request):
-#     return render (request,'design.html')
-
-# def hackathon(request):
-#     return render (request,'hackathon.html')
-
-# def Lan(request):
-#     return render (request,'Lan.html')
-
-# def Techcharades(request):
-#     return render (request,'Techcharades.html')
-
-# def Whizquiz(request):
-#     return render (request,'Whizquiz.html')
-
-# def Techcharades(request):
-#     return render (request,'Techcharades.html')
->>>>>>> 92e33cf4995f2c6bf555dca59557769aac4702bb
